e10b14917a585898074e1233d3e25e9e127ce69e
Rafael Matiola Arceno <96425404+rafael-ma@users.noreply.github.com>
2023-11-06 11:57:33 -0300

    Adicionada função principal do script
    Valores de input pegos e armazenados.

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ function toggleMode() {
     body.classList.toggle('light')
     switchButton.animate('animation', 'slideOut .2s ease')
 }
+
+function encript() {
+    const keyWord = document.querySelector('#keyWord').value
+    const repositionKey =document.querySelector('#repositionKey').value
+    const caractersNumber = document.querySelector('#charactersNumber').value
+    const specialCaracter = document.querySelector('#specialCharacter').value
+}
