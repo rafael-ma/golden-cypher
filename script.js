574f3bc0f0718c9e339c65760c2187c86ce50c3a
Rafael Matiola Arceno <96425404+rafael-ma@users.noreply.github.com>
2023-11-13 15:28:36 -0300

    Funções validadoras
    Criadas funções para verificar se o valor introduzido pelo usuário é válida para o processamento correto dos dados.

--- script.js
+++ script.js
@@ -6,9 +6,64 @@ function toggleMode() {
     switchButton.animate('animation', 'slideOut .2s ease')
 }
 
+function keyWordValidator(keyWord) {
+    if (keyWord == '') {
+        alert('Você deve inserir uma palavra chave')
+        document.querySelector('#keyWord').focus()
+        return true
+    } else {
+        return false
+    }
+}
+
+function repositionKeyValidator(repositionKey) {
+    if (repositionKey == '') {
+        alert('Você deve inserir chave de reposição.')
+        document.querySelector('#repositionKey').focus()
+        return true
+    } else {
+        return false 
+    }
+}
+
+function charactersNumberValidator(charactersNumber) {
+    if (charactersNumber == '') {
+        alert('Você deve inserir o número de caracteres.')
+        document.querySelector('#charactersNumber').focus()
+        return true
+    } else 
+    if ((charactersNumber < 4) || (charactersNumber > 24)) {
+        alert('Você deve informar um número de caracteres entre 4 e 24.')
+        document.querySelector('#charactersNumber').value = ''
+        document.querySelector('#charactersNumber').focus()
+        return true
+    } else {
+        return false
+    }
+}
+
+function specialCharacterValidator(specialCharacter){
+    let nonSpecial = 'abcdefghijklmnopqrstuvwxyz0123456789'
+    let error = false
+    for (let i = 0; i < 36; i++) {
+        if (specialCharacter == nonSpecial[i]) {
+            alert('Você deve informar um caractere do tipo especial. \nExemplo: @ # % & _')
+            document.querySelector('#specialCharacter').value = ''
+            document.querySelector('#specialCharacter').focus()
+            error = true
+        }
+    }
+    if (specialCharacter == '') {
+        document.querySelector('#specialCharacter').value = '-'
+    }
+    
+    return error
+}
+
 function encript() {
-    const keyWord = document.querySelector('#keyWord').value
-    const repositionKey =document.querySelector('#repositionKey').value
-    const caractersNumber = document.querySelector('#charactersNumber').value
-    const specialCaracter = document.querySelector('#specialCharacter').value
+    const keyWord = document.querySelector('#keyWord').value.toLowerCase()
+    const repositionKey = document.querySelector('#repositionKey').value.toLowerCase()
+    const charactersNumber = Number(document.querySelector('#charactersNumber').value)
+    const specialCharacter = document.querySelector('#specialCharacter').value.toLowerCase()
+    specialCharacterValidator(specialCharacter)
 }
